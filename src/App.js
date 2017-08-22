1e11957b3027e0c360b34a5cdf45b65c7e99c424
Isak La Fleur <isak.engdahl@gmail.com>
2017-08-22 20:35:02 +0200

    added react router and google maps

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 // Components
 import Navbar from './components/Navbar';
-
+import Home from './components/Home';
+import Test from './components/Test';
+import NotFound from './components/NoteFound';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <Navbar />
-      </MuiThemeProvider>
+      <BrowserRouter>
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+          <div>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/test" component={Test} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
-class Navbar extends Component {
+export class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+
+  handleToggle = () => this.setState({ open: !this.state.open });
+  handleClose = () => this.setState({ open: false });
+
   render() {
     return (
-      <AppBar title="Adventure Spots" iconClassNameRight="muidocs-icon-navigation-expand-more" />
+      <div>
+        <AppBar title="Adventure Spots" onLeftIconButtonTouchTap={this.handleToggle} />
+        <Drawer
+          docked={false}
+          open={this.state.open}
+          onRequestChange={open => this.setState({ open })}
+        >
+          <NavLink exact to="/">
+            <MenuItem onClick={this.handleClose}>Home</MenuItem>
+          </NavLink>
+          <NavLink to="/test">
+            <MenuItem onClick={this.handleClose}>Test</MenuItem>
+          </NavLink>
+        </Drawer>
+      </div>
     );
   }
 }
